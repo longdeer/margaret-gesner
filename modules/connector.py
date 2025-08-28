f4108a8cc99540c68b4961b0e2abfb603376fc96
lngd <lngdeer@gmail.com>
2025-08-28 14:59:11 +0300

    index tables menu prototype

--- modules/connector.py
+++ modules/connector.py
@@ -1,3 +1,47 @@
+from	os				import getenv
+from	typing			import Dict
+import	mysql.connector
+
+
+
+
+
+
+
+
+async def get_tables(rsrc :str, loggy) -> Dict[str,str] :
+
+	"""
+		rsrc	- request's ip address as string;
+		loggy	- Logger object (logging wrapper).
+	"""
+
+	response = dict()
+
+	try:
+
+		dbname = getenv("DB_NAME")
+		connection = mysql.connector.connect(
+
+			user=getenv("DB_USER_NAME"),
+			password=getenv("DB_USER_PASSWORD"),
+			host=getenv("DB_ADDRESS"),
+			database=dbname
+		)
+		session = connection.cursor()
+		loggy.debug(f"{dbname} connection established for {rsrc}")
+
+
+		session.execute("SELECT * FROM %s"%getenv("DB_STRUCTURE_TABLE"))
+		response = dict(session)
+
+
+		session.close()
+		connection.close()
+
+
+	except	mysql.connector.Error as E : loggy.error(f"{E.__class__.__name__}: {E}")
+	return	response
 
 
 
--- wsgi.py
+++ wsgi.py
@@ -1,10 +1,11 @@
-from os				import getenv
-from ops			import in_access_list
-from modules.logger	import Logger
-from flask			import Flask
-from flask			import request
-from flask			import render_template
-from dotenv			import load_dotenv
+from os					import getenv
+from ops				import in_access_list
+from modules.logger		import Logger
+from modules.connector	import get_tables
+from flask				import Flask
+from flask				import request
+from flask				import render_template
+from dotenv				import load_dotenv
 
 
 
@@ -25,10 +26,15 @@ loggy = Logger(getenv("LOGGY_FILE"), getenv("APP_NAME"), getenv("LOGGY_LEVEL"))
 
 
 @app.route("/")
-def index():
+async def index():
 
 	if	in_access_list(request.remote_addr, loggy):
-		return render_template("index.html")
+
+		tables = await get_tables(request.remote_addr, loggy)
+		names = sorted(tables)
+		aliases = [ tables[name] for name in names ]
+
+		return render_template("index.html", names=names, aliases=aliases)
 	return render_template("restricted.html")
 
 
